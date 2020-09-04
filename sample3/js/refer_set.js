8e67f78ff779b1077b62218f99da8bb0fe8040f8
Kamitotomi <rockyaaokage@gmail.com>
2020-09-04 13:17:58 +0900

    Haru

--- sample3/js/refer_set.js
+++ sample3/js/refer_set.js
@@ -11,3 +11,22 @@ let m = ['赤', '黄', '青'];
 let n = ['赤', '黄', '青'];
 console.log(m == n);
 // false
+
+let data = [85, 625, 124, 830, 227];
+let [x0, x1, x2, ...last] = data;
+
+console.log(x0);
+
+console.log(last);
+// 「...」で分割しきれなかった残りの要素をまとめる
+
+// 論理演算 左式がtrueである時点で全体の式はtrue右式は評価されない
+// let x = 50;
+// let y = 10;
+
+// console.log(x === 100 && y === 10);
+
+let name = '';
+name = name || '遥'
+console.log(name)
+
